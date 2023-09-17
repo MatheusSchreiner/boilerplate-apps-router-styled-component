e04bf31a6d2adb60e8ebb99b0ea178d354483a9a
Matheus Schreiner <matheusrubisch@gmail.com>
2023-09-17 17:57:19 -0300

    Styled-Components - Provendo CSS Global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import StyledComponentsRegistry from '@/lib/registry'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import StyledComponentsRegistry from '@/lib/registry'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <Providers>{children}</Providers>
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
